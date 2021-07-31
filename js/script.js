7b4310ebd2ff24e54a4fcca65fd6170ff48fcd27
Tomoki-i <nowvilla.physi.311@gmail.com>
2021-07-31 23:50:20 +0900

    add main page.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
-// $("").on("click", function () {
-//     return false;
-// })
+$(document).ready(function () {
+    const remainingDays = new RemainingDays(
+        new Date(2021, 7, 29)
+    );
+
+    const now = new Date();
+    remainingDays.calcRemainingDays(now)
+
+    const element = document.getElementById('day');
+    element.innerHTML = remainingDays.getRemainingDays();
+});
+
+class RemainingDays {
+    constructor(eventDate) {
+        this.eventDate = eventDate;
+    };
+
+    remainingDays = null;
+
+    calcRemainingDays(now) {
+        this.remainingDays = Math.floor(
+            (this.eventDate.getTime() - now.getTime()) / (24 * 60 * 60 * 1000) + 1
+        );
+    };
+
+    getRemainingDays() {
+        return this.remainingDays;
+    };
+}
