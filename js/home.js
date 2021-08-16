931e54e3792300ef7a4f1ad5dacf2a18c8b339ad
Tomoki-i <nowvilla.physi.311@gmail.com>
2021-08-16 22:31:40 +0900

    update href.

--- js/home.js
+++ js/home.js
@@ -35,5 +35,5 @@ class RemainingDays {
 }
 
 $('.reserve-btn').on('click', function () {
-    window.location.href = '../FireWork/reserve.html';
+    window.location.href = './FireWork/reserve.html';
 });
